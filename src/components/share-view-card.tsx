1c0524095358e9497e65099f632234d811154845
Julian <130256240+j4n-e4t@users.noreply.github.com>
2025-01-16 22:19:21 +0100

    implement better error handling

--- src/components/share-view-card.tsx
+++ src/components/share-view-card.tsx
@@ -9,40 +9,45 @@ import {
 import { CopyShareContentButton } from "./copy-share-button";
 import { api } from "@/trpc/server";
 import { Skeleton } from "./ui/skeleton";
+import { notFound } from "next/navigation";
 
 export default async function ShareViewCard({ code }: { code: string }) {
-  const share = await api.share.get({ code });
+  try {
+    const share = await api.share.get({ code });
 
-  return (
-    <Card className="w-full max-w-md">
-      <CardHeader>
-        <CardTitle>{share.title}</CardTitle>
-        <CardDescription>
-          {new Date(share.created_at).toLocaleDateString("en-US", {
-            dateStyle: "long",
-          })}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p className="whitespace-pre-wrap break-words rounded-md bg-muted p-4">
-          {share.content.startsWith("http") ? (
-            <a
-              href={share.content}
-              target="blank"
-              className="text-primary underline"
-            >
-              {share.content}
-            </a>
-          ) : (
-            share.content
-          )}
-        </p>
-      </CardContent>
-      <CardFooter className="flex justify-end">
-        <CopyShareContentButton share={share} />
-      </CardFooter>
-    </Card>
-  );
+    return (
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>{share.title}</CardTitle>
+          <CardDescription>
+            {new Date(share.created_at).toLocaleDateString("en-US", {
+              dateStyle: "long",
+            })}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="whitespace-pre-wrap break-words rounded-md bg-muted p-4">
+            {share.content.startsWith("http") ? (
+              <a
+                href={share.content}
+                target="blank"
+                className="text-primary underline"
+              >
+                {share.content}
+              </a>
+            ) : (
+              share.content
+            )}
+          </p>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <CopyShareContentButton share={share} />
+        </CardFooter>
+      </Card>
+    );
+  } catch (error) {
+    notFound();
+  }
 }
 
 export function ShareViewCardSkeleton() {
