ba818c641d8d94bc9f86196069b3427bfa6a31f6
Julian <130256240+j4n-e4t@users.noreply.github.com>
2025-04-07 17:26:29 +0200

    Add Badge component and update NavBar to include version badge

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,15 +1,19 @@
 import { Github } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { Badge } from "./ui/badge";
 
 export default function NavBar() {
   return (
-    <nav className="flex items-center justify-between bg-background p-4">
+    <nav className="bg-background flex items-center justify-between p-4">
       <Link href="/">
-        <span className="text-2xl font-bold">
+        <div className="flex items-center justify-start gap-2 text-2xl font-bold">
           Quick
           <span className="text-primary">Share</span>
-        </span>
+          <Badge className="text-sm" variant="outline">
+            V1
+          </Badge>
+        </div>
       </Link>
       <div className="flex items-center gap-4">
         <Link target="_blank" href="https://github.com/j4n-e4t/quick-share">
